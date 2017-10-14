1f32eab105a15166165074d68a2dc065557dc301
pjmaddox24 <pjmaddox24@gmail.com>
2017-10-14 00:38:31 -0500

    Videos 7&8 - Input and Raycasts

---
+++ UnityFpsStepByStep/Assets/Scripts/Chapter 1/Shoot.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chapter1
+{
+    public class Shoot : MonoBehaviour
+    {
+
+        float fireRate = 0.3f;
+        float nextTime;
+        RaycastHit hit;
+        float range = 300f;
+        Transform myTransform;
+
+        // Use this for initialization
+        void Start()
+        {
+            SetInitialReferences();
+        }
+
+        void SetInitialReferences()
+        {
+            myTransform = this.transform;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            CheckForInput();   
+        }
+        void CheckForInput()
+        {
+            if (Input.GetKey(KeyCode.Mouse0) && Time.time >= nextTime)
+            {
+                Fire();
+                nextTime = Time.time + fireRate;
+            }
+        }
+        void Fire()
+        {
+            if(Physics.Raycast(myTransform.TransformPoint(0,0,1), myTransform.forward, out hit, range))
+            {
+                Debug.DrawRay(myTransform.TransformPoint(0,0,1), myTransform.forward, Color.cyan, 2f);
+                Debug.Log(hit.transform.name);
+            }
+        }
+    }
+}
---
+++ UnityFpsStepByStep/Assets/Scripts/Chapter 1/Welcomer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Welcomer : MonoBehaviour {
+
+    public string MyMessage = "";
+    private Text textWelcome;
+    public float canvasDisableWaitTime = 3f;
+    public GameObject canvasWelcome;
+
+	// Use this for initialization
+	void Start () {
+        SetInitialReferences();
+        DisplayWelcomeMessage();
+	}
+
+    void SetInitialReferences()
+    {
+        textWelcome = GameObject.Find("TextWelcome").GetComponent<Text>();
+        Debug.Log(canvasWelcome);
+    }
+
+	void DisplayWelcomeMessage()
+    {
+        if (textWelcome == null)
+        {
+            Debug.LogWarning("Welcome text not assigned");
+            return;
+        }
+        textWelcome.text = MyMessage;
+
+        //Option 1
+        //Invoke("DisableCanvas", 3f);
+
+        //Option 2 - Better
+        StartCoroutine(DisableCanvas(canvasDisableWaitTime));
+    }
+    IEnumerator DisableCanvas(float timeToWait)
+    {
+        yield return new WaitForSeconds(timeToWait);
+        canvasWelcome.SetActive(false);
+    }
+}
